1bac32f60d2583e670c5217ef297b7c56e69e245
William David Edwards <wedwards@cyberfusion.nl>
2025-12-14 13:04:31 +0100

    Update to Trixie

--- src/cyberfusion/ProftpdSupport/settings.py
+++ src/cyberfusion/ProftpdSupport/settings.py
@@ -1,16 +1,15 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="proftpd_support_", env_file=(".env", "/etc/proftpd-support.conf")
+    )
+
     database_path: Path
 
     user_expire_hours: int = 1
-
-    class Config:
-        env_prefix = "proftpd_support_"
-
-        env_file = ".env", "/etc/proftpd-support.conf"
 
 
 settings = Settings()
